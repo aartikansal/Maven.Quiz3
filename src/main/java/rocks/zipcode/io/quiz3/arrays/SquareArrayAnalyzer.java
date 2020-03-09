cac1e7f93cd325d7a72771ba3f76ed273a269510
aartikansal <aartilumb@gmail.com>
2020-03-09 10:00:04 -0400

    first two sections of part 1 done

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -14,6 +14,9 @@ public class SquareArrayAnalyzer {
      * @return true if each `inputArray[i]` has a corresponding `squaredValues[j]` such that `squaredValues[j] == inputArray[i] * inputArray[i]`
      */
     public static Boolean compare(Integer[] inputArray, Integer[] squaredValues) {
-        return null;
+        if (inputArray.equals(squaredValues)){
+            return false;
+        }
+        return true;
     }
 }
